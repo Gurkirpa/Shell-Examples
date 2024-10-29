df0899c99088923d1f5c586ad31aa7ef1c0037f2
Gurkirpa <jasneet3637@gmail.com>
2024-10-29 06:36:37 +0600

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+*****************************
+Author:- Amanjot
+Project:-Shell-Script
+Date:-28-oct-2024
+*****************************
+
 for i in {1..100}; do
 if ([ `expr $i % 3` == 0 ] || [ `expr $i % 5` == 0 ]) && [ `expr $i % 15` != 0 ];
 then
