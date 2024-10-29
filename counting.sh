2c476bb5222054e8cf7ff4c1a594e2218eea0d5f
Gurkirpa <jasneet3637@gmail.com>
2024-10-29 06:37:04 +0600

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+*****************************
+Author:- Amanjot
+Project:-Shell-Script
+Date:-28-oct-2024
+*****************************
+
 x=missisippi
 
 grep -o "p" <<< $x | wc -l
